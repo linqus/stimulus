b9bcc7aab223c74a23cac575c05720e0d8899168
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-20 20:21:33 +0200

    show incremented counter after click

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -3,6 +3,12 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
 
     connect() {
-        this.element.innerHTML = 'You have clicked 0 times';
+        this.element.innerHTML = 'You have clicked 0 times ;(';
+
+        this.count = 0;
+        this.element.addEventListener('click', () => {
+            this.count++;
+            this.element.innerHTML = 'You have clicked ' + this.count + ' times';
+        } )
     }
 }
