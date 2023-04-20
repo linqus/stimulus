5a8fbf5bdabac054e80003a41e21a82fc2eeaf51
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-20 21:00:46 +0200

    event to button dirty way

--- assets/controllers/counter_controller.js
+++ assets/controllers/counter_controller.js
@@ -1,13 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = ['count'];
+    static targets = [
+        'count',
+        'clickMeButton',
+    ];
 
     connect() {
 
         this.count = 0;
 
-        this.element.addEventListener('click', () => {
+        this.clickMeButtonTarget.addEventListener('click', () => {
             this.count++;
             this.countTarget.innerHTML = this.count;
         } )
